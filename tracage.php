8276c8785eb9638610cd73c376afde40a12db40b
ZeCrusher <102222839+ZeCrusher@users.noreply.github.com>
2024-09-10 12:27:27 +0200

    Update tracage.php
    Date dans le nom du fichier log.

--- tracage.php
+++ tracage.php
@@ -1,6 +1,7 @@
 <?		
+
 // 		┌───────────────────────────────────────────────────────────────────────────────────────────────┐
-// 		│ tracage : Création d'un fichier dans /log/tracage.log qui sert de traceur AJAX         		│
+// 		│ tracage : Création d'un fichier dans /log/tracage.log qui sert de traceur AJAX     		│
 // 		└───────────────────────────────────────────────────────────────────────────────────────────────┘
 	
 	function tracage($texte) {
@@ -11,8 +12,11 @@
 		}
 		$txt_log=$adresse_ip.';'.date('d/m/Y H:i:s').';'.$texte."\n";
 		// écriture dans un fichier de traçage
-		// $fichier = "log/tracage_".date('Ymd').".log";
-		$fichier = "tracage.log";
+		$fichier = "log/tracage_".date('Ymd').".log";
+
+		// Vous pouvez faire soit un seul fichier / soit un fichier par jour avec date('Ymd') dans le nom du fichier.
+		// $fichier = "/log/tracage.log";
+		
 		preg_match("`^(.*\/)([^\/]+)$`",$_SERVER['SCRIPT_FILENAME'], $matches);
 		$chemin_script = $matches[1];
 		$fichierCible = $chemin_script.$fichier;
