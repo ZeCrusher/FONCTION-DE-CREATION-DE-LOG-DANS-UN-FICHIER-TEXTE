3efcede21d2a1fc333f8f8170f88a44f0350d654
ZeCrusher <102222839+ZeCrusher@users.noreply.github.com>
2024-09-10 12:00:44 +0200

    Create tracage.php
    Création du fichier PHP.
    Version 1 !

---
+++ tracage.php
@@ -0,0 +1,24 @@
+<?		
+// 		┌───────────────────────────────────────────────────────────────────────────────────────────────┐
+// 		│ tracage : Création d'un fichier dans /log/tracage.log qui sert de traceur AJAX         		│
+// 		└───────────────────────────────────────────────────────────────────────────────────────────────┘
+	
+	function tracage($texte) {
+		// Si on peut déterminer l'adresse IP
+		$adresse_ip = Null;
+		if(isset($_SERVER['REMOTE_ADDR'])) {
+			$adresse_ip = '"'.$_SERVER['REMOTE_ADDR'].'"';
+		}
+		$txt_log=$adresse_ip.';'.date('d/m/Y H:i:s').';'.$texte."\n";
+		// écriture dans un fichier de traçage
+		// $fichier = "log/tracage_".date('Ymd').".log";
+		$fichier = "tracage.log";
+		preg_match("`^(.*\/)([^\/]+)$`",$_SERVER['SCRIPT_FILENAME'], $matches);
+		$chemin_script = $matches[1];
+		$fichierCible = $chemin_script.$fichier;
+		$myFile=fopen($fichierCible,'a+');
+		fputs($myFile,$txt_log);
+		fclose($myFile);
+	}
+
+?>	
